72ddda592f215ff49f6ec91b0419b014e989e795
tangyali <1985698935@qq.com>
2023-11-28 00:05:26 +0800

    浏览器窗口大小设置

---
+++ web自动化/7-浏览器前进、后退、关闭、退出.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+# 打开浏览器
+driver = webdriver.Chrome()
+
+# 请求百度
+time.sleep(2)
+driver.get('https://www.baidu.com')
+
+# TODO 浏览器后退
+time.sleep(2)
+driver.back()
+# TODO 浏览器前进
+time.sleep(2)
+driver.forward()
+# TODO 浏览器刷新
+time.sleep(2)
+driver.refresh()
+# TODO 关闭浏览器当前窗口
+driver.close()
+# TODO 退出浏览器
+driver.quit()
+
+
+
+
+
